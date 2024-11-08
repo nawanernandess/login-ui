5dba999f8f3feb7b62a5c4ef520c3296e1dfced2
Nawan Ernandes <nawanernandess@gmail.com>
2024-11-08 00:01:09 -0300

    Card
    -- Cria componente de card;
    -- Remove .html do card;
    -- Passa configuração e estilo para o componente;

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faLock, faUser } from '@fortawesome/free-solid-svg-icons';
+import { CardComponent } from '../../shared/card/card.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FontAwesomeModule],
+  imports: [CommonModule, FontAwesomeModule, CardComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
