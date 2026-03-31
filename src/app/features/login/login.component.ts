41ae53ed26bf9ff268540dc59b1bc46bfdaee2aa
Nawan Ernandes <nawanernandess@gmail.com>
2026-03-30 23:57:32 -0300

    feat(login): cria tela de login com formulário reativo e layout split-screen

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,27 +1,41 @@
-import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import {
   FormBuilder,
   FormGroup,
   Validators,
   ReactiveFormsModule,
 } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-login',
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatIconModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-  myForm: FormGroup;
-  formValidation: boolean = false;
+  readonly hidePassword = signal(true);
+  readonly activeSlide = signal(0);
+  readonly totalSlides = 3;
 
-  private readonly _formBuilder = inject(FormBuilder);
+  loginForm: FormGroup;
+
+  private readonly _fb = inject(FormBuilder);
 
   constructor() {
-    this.myForm = this._formBuilder.group({
-      login: ['', [Validators.required, Validators.minLength(3)]],
+    this.loginForm = this._fb.group({
+      email: ['', [Validators.required, Validators.email]],
       password: [
         '',
         [
@@ -30,15 +44,24 @@ export class LoginComponent {
           Validators.maxLength(16),
         ],
       ],
+      keepSignedIn: [false],
     });
   }
 
-  submit() {
-    if (!this.myForm.valid) {
-      return console.log('invalido!!');
+  togglePasswordVisibility(): void {
+    this.hidePassword.update((v) => !v);
+  }
+
+  setSlide(index: number): void {
+    this.activeSlide.set(index);
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
     }
 
-    console.log('valido');
-    this.formValidation = true;
+    console.log('Login válido:', this.loginForm.value);
   }
 }
